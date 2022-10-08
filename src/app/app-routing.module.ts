5afb54c5f3b47bc7fc0e2ca0e35aaa05b4dff8a5
mauropomar <mauropomardurruthy@gmail.com>
2022-10-08 14:41:02 -0500

    Configure the routes and the vehicle module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'',
+  pathMatch:'full',
+  redirectTo:'vehiculos'
+},{
+  path:'vehiculos',
+  loadChildren:()=>
+    import('./components/vehiculo/vehiculo.module').then((m)=>m.VehiculoModule)
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/vehiculo/vehiculo-routing.module.ts
+++ src/app/components/vehiculo/vehiculo-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaVehiculoComponent } from './lista/lista-vehiculo.component';
+import { FormVehiculoComponent } from './form/form-vehiculo.component';
+import { DetalleVehiculoComponent } from './detalle/detalle-vehiculo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:ListaVehiculoComponent
+  },{
+    path:'lista',
+    component:ListaVehiculoComponent
+  },{
+    path:'nuevo',
+    component:FormVehiculoComponent
+  },{
+    path:'editar/:id',
+    component:FormVehiculoComponent
+  },{
+    path:'detalle/:id',
+    component:DetalleVehiculoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
