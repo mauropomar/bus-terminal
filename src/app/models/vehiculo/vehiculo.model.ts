9be5b1abc4081064e42305531b1ecf698de9ca5b
mauropomar <mauropomardurruthy@gmail.com>
2022-10-08 13:17:45 -0500

    Create services and model

---
+++ src/app/models/vehiculo/vehiculo.model.ts
@@ -0,0 +1,11 @@
+export interface VehiculoModel{
+    id?:string;
+    placa:string;
+    capacidad: number;
+    tipoUnidad:string;
+    foto?:string;
+    fechaVencimientoSoat?:string;
+    fechaVencimientoRevisionTecnica?:string;
+    fechaRevisionTecnica?:string;
+    fotoSoat?:string;
+}
---
+++ src/app/services/vehiculo/vehiculo.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; 
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class VehiculoService {
+
+   constructor(private http: HttpClient) {
+        
+    }
+
+    public getJSON(): Observable<any> {
+        return this.http.get("./assets/data/vehiculos.json");
+    }
+}
